0435ad216ff353f281ca84b6282154c3392a5a59
rayanc <rayanc@kth.se>
2023-08-25 18:36:27 +0200

    done

---
+++ src/filter.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg import Image
+from Norbit_FLS_driver.msg import Fls
+import numpy as np
+import cv2
+import time
+import math
+
+__author__ = "Rayan Cali"
+__author_email = "rayanc@kth.se"
+
+def image_callback(msg):
+    global queue, amount_of_pics, pub, sub_goal,done_once,iteration, width, height, fused
+        #delete the first element and add the new one
+    t = time.time()
+    bin_max = ((msg.t0 + msg.num_samples)*msg.snd_velocity)/(2*msg.sample_rate)
+    if len(queue) < amount_of_pics:
+        
+        if height != msg.num_samples or width != msg.num_beams:
+            queue = []
+            fused.width = msg.num_beams
+            fused.height = msg.num_samples
+            fused.step = msg.num_beams
+        height = msg.num_samples
+        width = msg.num_beams
+        imagearray = np.array(msg.fls_raw.data[:-width])
+        imagearray = np.reshape(imagearray, (height,width))
+        cutoff_perc = float(rospy.get_param("/cutoff_percentage"))
+        imagearray[0:int(height*cutoff_perc), :] = 0
+        # imagearray[:, 0:int(width*0.3)] = 0
+        imagearray, vgamask=remove_gains(imagearray, msg.gain, msg.vga_t1,msg.vga_t2,msg.vga_g1,msg.vga_g2,msg.snd_velocity, msg.tx_freq, int(height*cutoff_perc), bin_max)
+        queue.append(imagearray)
+    if len(queue) == amount_of_pics:
+        fused_pic = fuse_the_pics(vgamask, msg.gain)
+        fused_stretched = linear_stretch(fused_pic)
+        median_filtered_pic = median_filtered(fused_stretched)
+        otsu = otsu_algorithm(median_filtered_pic)
+        morphed = morphological_operations(otsu)
+        
+   
+        del queue[0]
+    
+    elapsed = time.time() - t
+    rospy.loginfo(elapsed)
+    return
+
+
+def remove_gains(imagearray, gain, t1, t2, g1, g2, snd_vel, freq, start_range, max_range):
+    global gainc, height
+    vgamask = np.zeros(imagearray.shape[0])
+    vgamask[:t1] = g1
+    vgamask[t2:] = g2
+    vgamask[t1:t2] = np.linspace(g1,g2,t2-t1)
+    vgamask = vgamask.transpose()
+    f= lambda x: 10**(x/20)
+    vgamask = f(vgamask)
+    if rospy.get_param("/remove_vga"):
+        imagearray = (imagearray.transpose()/vgamask).transpose()
+    imagearray = imagearray/((10**gain)/20)
+    if rospy.get_param("/use_self_calc_gain"):
+        start_range = start_range*max_range/height
+        viscosity = 1.18e-3 #viscosity of seawater 15 degrees celcius
+        angfreq = 2*math.pi*freq*1e3 #angular frequency
+        density = 1020 #density of seawater
+        ampllist = np.ones(imagearray.shape[0])*2*viscosity*angfreq**2/(3*density*snd_vel**3)
+        rangelist = np.linspace(start_range,max_range,imagearray.shape[0])*2
+        ampllist = ampllist*rangelist**2
+        imagearray = (imagearray.transpose()*ampllist).transpose()
+        absorbcoeff = 0.06388 #absorption coefficient of seawater 15 degrees celcius, calculated with 10ppt salinity, PH 8, depth 50m, and freq 400kHz,
+        absorb = rangelist*absorbcoeff
+        absorb = f(absorb)
+        imagearray = (imagearray.transpose()*absorb).transpose()
+
+    max_value = np.max(imagearray)
+    imagearray_scaled = (255/max_value)*imagearray
+    imagearray_int = imagearray_scaled.astype(np.uint8)    
+    fused.data = imagearray_int.flatten().tolist()
+    pub0.publish(fused)
+    return imagearray,vgamask
+
+
+
+def fuse_the_pics(vgamask,gain):
+    global queue,amount_of_pics, pub, sub_goal,done_once,fused
+    summed_pics = np.sum(queue, axis=0)
+    summed_pics = summed_pics*((10**gain)/20)
+    if rospy.get_param("/use_log"):
+        summed_pics_nozeros = summed_pics[summed_pics != 0]
+        summed_pics_nozeros = np.log(summed_pics_nozeros)
+        sorted_summed_pics_non_zeros = -np.sort(-summed_pics_nozeros, axis=None)
+    
+    
+        top1percent_summed_pics = sorted_summed_pics_non_zeros[:int(len(sorted_summed_pics_non_zeros)*0.01)]
+        th = np.sum(top1percent_summed_pics)/(len(top1percent_summed_pics)*amount_of_pics)
+        summed_pics = np.log(summed_pics)
+        summed_pics[summed_pics <= th] = -np.inf  
+        summed_pics = np.exp(summed_pics)
+    else: 
+        sorted_summed_pics = -np.sort(-summed_pics, axis=None)
+        sorted_summed_pics_non_zeros = sorted_summed_pics[sorted_summed_pics != 0]
+        top1percent_summed_pics = sorted_summed_pics_non_zeros[:int(len(sorted_summed_pics_non_zeros)*0.01)]
+        th = np.sum(top1percent_summed_pics)/(len(top1percent_summed_pics)*amount_of_pics)
+        summed_pics[summed_pics <= th] = 0
+    
+    if rospy.get_param("/remove_vga") and not rospy.get_param("/use_self_calc_gain"):
+        summed_pics = (summed_pics.transpose()*vgamask).transpose()
+    
+    max_value = np.max(summed_pics)#sorted_summed_pics_non_zeros[0]
+    fused_pic = (255/max_value)*summed_pics
+    fused_pic_int = fused_pic.astype(np.uint8)
+    
+ 
+    fused.data = fused_pic_int.flatten().tolist()
+    pub.publish(fused)
+   
+    return fused_pic
+
+def linear_stretch(fused_pic):
+    global fused, pub2
+    flat_sorted = np.sort(fused_pic, axis=None)
+    flat_sorted = flat_sorted[flat_sorted != 0]
+    
+    clipped_flat_sorted = flat_sorted[int(0.02*len(flat_sorted)):int(0.98*len(flat_sorted))]
+    max_value = clipped_flat_sorted[-1]
+    min_value = clipped_flat_sorted[0]
+
+    f = lambda x: 255*(x-min_value)/(max_value-min_value)
+    fused_pic[fused_pic<min_value] = min_value
+    fused_pic[fused_pic>max_value] = max_value
+    contrast_stretched = f(fused_pic)
+    fused_pic_int = contrast_stretched.astype(np.uint8)
+
+    fused.data = fused_pic_int.flatten().tolist()
+    pub2.publish(fused)
+    return contrast_stretched
+
+def median_filtered(pic):
+    global fused,pub3
+    cv_pic = pic.astype(np.uint8)
+    median_filtered_pic = cv2.medianBlur(cv_pic, rospy.get_param("/size_medianfilter"))
+    fused.data = median_filtered_pic.flatten().tolist()
+    pub3.publish(fused)
+    return median_filtered_pic
+   
+def otsu_algorithm(pic):
+    global fused,pub4
+
+    ret, otsu = cv2.threshold(pic.astype(np.uint8),0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+  
+    fused.data = otsu.flatten().tolist()
+    pub4.publish(fused)
+    return otsu
+def morphological_operations(pic):
+    global fused,pub5
+    size = int(rospy.get_param("/morph_size"))
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (size, size))
+    dilated = cv2.dilate(pic,kernel,iterations = 1)
+    eroded = cv2.erode(dilated,kernel,iterations = 1)
+    morphed = eroded
+  
+    
+    fused.data = morphed.flatten().tolist()
+
+    pub5.publish(fused)
+    return morphed
+
+    
+
+    
+
+
+
+rospy.init_node('filter')
+
+sub_goal = rospy.Subscriber('/fls/data', Fls, image_callback)
+
+pub0 = rospy.Publisher('/gain_removed', Image, queue_size=1)
+pub = rospy.Publisher('/fused', Image, queue_size=1)
+pub2 = rospy.Publisher('/fused_and_stretched', Image, queue_size=1)
+pub3 = rospy.Publisher('/median_filtered', Image, queue_size=1)
+pub4 = rospy.Publisher('/otsu', Image, queue_size=1)
+pub5 = rospy.Publisher('/morphed', Image, queue_size=1)
+
+amount_of_pics = rospy.get_param("/nr_of_pics")
+queue = []
+iteration = 0
+gainc= 1
+done_once = False
+width = 256
+height = 650
+fused = Image()
+fused.height = height
+fused.width = width
+fused.encoding = 'mono8'
+fused.step = width
+fused.header.frame_id = ''
+fused.is_bigendian = 0
+if __name__ == '__main__':
+   
+    rospy.spin()
---
+++ src/grid.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import TransformStamped
+import tf2_ros, tf
+import tf2_geometry_msgs
+import math
+from sensor_msgs.msg import Image
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped
+import time
+from Norbit_FLS_driver.msg import Fls
+__author__ = "Rayan Cali"
+__author_email = "rayanc@kth.se"
+
+
+def image_callback(msg: Image):
+    global fade_factor, pub, sub_goal,first_image,grid,beam_min,start,bin_max,tfBuffer,listener,done_once, range_min,directions, prev_matrix, base_pos, trans_base
+    t = time.time()
+    cv_image = CvBridge().imgmsg_to_cv2(msg, "mono8")
+    if rospy.get_param('/flip_image'):
+        cv_image = cv2.flip(cv_image, 1)
+    if done_once == False:
+        matrix = np.zeros((grid.info.height,grid.info.width), dtype=np.int8)
+    else:
+        matrix = -fade_factor*np.ones((grid.info.height,grid.info.width), dtype=np.int8)
+    trans = tfBuffer.lookup_transform("odom", "fls_link", rospy.Time(0))
+    trans2 = tfBuffer.lookup_transform("odom", "base_link", rospy.Time(0))
+    trans_base = tfBuffer.lookup_transform("base_link", "odom", rospy.Time(0)) # for use in update_matrix()
+    anglelist = trans2.transform.rotation.x, trans2.transform.rotation.y, trans2.transform.rotation.z, trans2.transform.rotation.w
+    yaw = tf.transformations.euler_from_quaternion(anglelist)[2]
+    imagearray = np.array(cv_image)
+    arr = np.where(imagearray == 255)
+    step_size_range = bin_max/(msg.height)
+  
+    trlist = trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z
+    rotlist = trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w
+    mat = tf.TransformerROS().fromTranslationRotation(trlist, rotlist)
+
+    ranges = range_min + arr[0]*step_size_range
+    angle = directions[arr[1]]
+   
+    z = ranges*np.cos(angle)
+    x = ranges*np.sin(angle)
+    transform_this = np.zeros((mat.shape[0],len(arr[0])))
+    transform_this[0][:] = x 
+    transform_this[2][:] = z
+    transform_this[3][:] = np.ones(len(arr[0]))
+   
+    transform_this = np.matmul(mat,transform_this)
+    xpos = np.cos(yaw)
+    ypos = np.sin(yaw)
+    diffs = (transform_this[0,:]-trans2.transform.translation.x) * xpos + (transform_this[1,:]-trans2.transform.translation.y) * ypos
+    transform_this[0,:] = diffs
+    coordinates = np.delete(transform_this, [1,3], axis=0).transpose()
+    coordinates[:, 1] = -(coordinates[:, 1] - trans2.transform.translation.z)
+    coordinates=coordinates*1/grid.info.resolution + base_pos
+    coordinates = coordinates.astype(int)
+    coordinates = np.delete(coordinates, np.where(coordinates<0)[0], axis=0)
+    coordinates = np.delete(coordinates, np.where((coordinates[:,1])>grid.info.height-1)[0], axis=0)
+    coordinates = np.delete(coordinates, np.where((coordinates[:,0])>grid.info.width-1)[0], axis=0)
+    
+    
+    
+    matrix[coordinates[:, 1], coordinates[:, 0]] = fade_factor
+    matrix[base_pos][base_pos] = fade_factor #mark where base link is on grid
+    rospy.loginfo(fade_factor)
+    if done_once == True:
+        matrix = matrix + prev_matrix
+        matrix = np.clip(matrix, 0, 100)
+        prev_matrix = matrix
+    else:
+        done_once = True
+        prev_matrix = matrix
+
+    
+    grid.data = matrix.flatten().tolist()
+
+ 
+    pub.publish(grid)
+    elapsed = time.time() - t 
+    rospy.loginfo(elapsed)
+    start = True
+
+
+def fls_callback(msg: Fls):
+    global directions, bin_max
+    directions = np.array(msg.fls_raw.data[-msg.num_beams:])
+    bin_max = ((msg.t0 + msg.num_samples)*msg.snd_velocity)/(2*msg.sample_rate)
+
+    
+
+def update_matrix(): #checks if matrix need to be rolled forward or backward depending on how much base_link has moved since latest image
+    global matrix, prev_matrix, trans_base, tfBuffer,grid,base_pos,start
+    if start:
+        t = time.time()
+        base_val = prev_matrix[base_pos,base_pos]
+        try:
+            current_base = tfBuffer.lookup_transform("odom", "base_link", rospy.Time(0))
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rospy.loginfo("no transform")
+        diffpose = PoseStamped()
+        diffpose.header.frame_id = "odom"
+        diffpose.pose.position.x,diffpose.pose.position.y, diffpose.pose.position.z = current_base.transform.translation.x, current_base.transform.translation.y, current_base.transform.translation.z
+        diffpose.pose.orientation.x, diffpose.pose.orientation.y,diffpose.pose.orientation.z,diffpose.pose.orientation.w = current_base.transform.rotation.x, current_base.transform.rotation.y, current_base.transform.rotation.z, current_base.transform.rotation.w
+        diffpose = tf2_geometry_msgs.do_transform_pose(diffpose, trans_base)
+        anglelist = diffpose.pose.orientation.x, diffpose.pose.orientation.y, diffpose.pose.orientation.z, diffpose.pose.orientation.w
+        yaw = tf.transformations.euler_from_quaternion(anglelist)[2]
+        if abs(yaw)<0.017: #1 degree
+            dist = abs(int(diffpose.pose.position.x/grid.info.resolution))
+            if diffpose.pose.position.x > grid.info.resolution:
+                trans_base = tfBuffer.lookup_transform("base_link", "odom", rospy.Time(0))
+                prev_matrix = np.roll(prev_matrix, -dist, axis=1)
+                prev_matrix[:, -dist:] = 0
+                prev_matrix[base_pos, (base_pos-dist)] = 0
+                prev_matrix[base_pos,base_pos] = base_val
+                rospy.loginfo("rolled fwd")
+                trans_base = tfBuffer.lookup_transform("base_link", "odom", rospy.Time(0))
+                
+            elif diffpose.pose.position.x < -grid.info.resolution:
+                trans_base = tfBuffer.lookup_transform("base_link", "odom", rospy.Time(0))
+                prev_matrix = np.roll(prev_matrix, dist, axis=1)
+                prev_matrix[:, 0:dist] = 0
+                prev_matrix[base_pos,(base_pos+dist)] = 0
+                prev_matrix[base_pos,base_pos] = base_val
+                rospy.loginfo("rolled bwd")
+                trans_base = tfBuffer.lookup_transform("base_link", "odom", rospy.Time(0))
+        elapsed = time.time() - t
+        # rospy.loginfo(elapsed)
+
+    
+
+
+
+rospy.init_node('grid')
+done_once = False
+sub_goal = rospy.Subscriber('/morphed', Image, image_callback)
+fls_sub = rospy.Subscriber('/fls/data', Fls, fls_callback)
+grid = OccupancyGrid()
+pub = rospy.Publisher('/grid', OccupancyGrid, queue_size=1)
+pub_flip = rospy.Publisher('/pic_flip', Image, queue_size=1)
+first_image = True
+bin_max = 200 # 100m is the max range of the sensor
+beam_min = -math.pi/2 # 180 degrees is the fov of the sensor
+tfBuffer = tf2_ros.Buffer()
+listener = tf2_ros.TransformListener(tfBuffer)
+st = tf2_ros.StaticTransformBroadcaster()
+prev_matrix = None
+base_pos = 10
+range_min = 0
+directions = None
+trans_base = None
+start = False
+fade_factor = rospy.get_param('/fade_factor')
+
+
+
+if __name__ == '__main__':
+    grid.header.frame_id = "odom"
+    grid.info.resolution = rospy.get_param('/grid_resolution')
+    height = rospy.get_param('/grid_height')
+    width = rospy.get_param('/grid_width')
+    grid.info.height = int(height*1/grid.info.resolution)
+    grid.info.width = int(width*1/grid.info.resolution)
+    grid.info.origin = Pose(Point(0,0,0),Quaternion(0,0,0,1)) # rotated 90 degrees
+    # t = TransformStamped()
+    # t.header.frame_id = "base_link"
+    # t.child_frame_id = "fls_link"
+    # t.transform.translation.x = 0
+    # t.transform.translation.y = 0
+    # t.transform.translation.z = -1
+    # t.transform.rotation.x = 0.923
+    # t.transform.rotation.y = 0.0
+    # t.transform.rotation.z = 0.383
+    # t.transform.rotation.w = 0
+    # st.sendTransform(t)
+    base_pos = int(base_pos/grid.info.resolution)
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        update_matrix() 
+        rate.sleep()
+    rospy.spin()
